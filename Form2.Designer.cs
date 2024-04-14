05b27d06b2277bbdc97bead5802c626bb5c48e46
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2024-04-14 15:32:38 +0700

    Add project files.

---
+++ Form2.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace MDIForm
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(119, 108);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(347, 217);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Pilih salah satu";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(66, 134);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(190, 24);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Kuliah di pantai/gunung";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(66, 93);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(120, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Kuliah di cafe";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(66, 49);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(171, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Kuliah di dalam kelas";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(119, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Pilihan anda:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(192, 340);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(274, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(128, 343);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Nama";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(372, 390);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Proses";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton radioButton1;
+        private Label label1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDIForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string radioButtonSelected = "";
+            if (radioButton1.Checked)
+            {
+                radioButtonSelected = radioButton1.Text;
+            }
+            else if (radioButton2.Checked)
+            {
+                radioButtonSelected = radioButton2.Text;
+            }
+            else if (radioButton3.Checked)
+            {
+                radioButtonSelected = radioButton3.Text;
+            }
+            string textBox = textBox1.Text;
+
+            label1.Text = $"{textBox1.Text} telah memilih {radioButtonSelected}";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
